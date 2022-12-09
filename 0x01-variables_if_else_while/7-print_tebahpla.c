331cc545918660369d2880f448cf5e793e20b0f6
Fash-ya <ishakufaith20@gmail.com>
2022-12-09 13:48:54 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print all lowercase alphabets in reverse
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabets;
+
+	for (alphabets = 'z'; alphabets >= 'a'; alphabets--)
+		putchar(alphabets);
+
+	putchar('\n');
+
+	return (0);
+}
