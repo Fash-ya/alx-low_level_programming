ff5879b1ffe34a1b251135de7b8386e275d4c582
Fash-ya <ishakufaith20@gmail.com>
2022-12-09 17:34:20 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,21 @@
 int main(void)
 {
 
-	int num1 = '0';
-	int num2 = '0';
+	int num1, num2;
 
-	for (num1 = '0'; num1 <= '9'; num1++)
+	for (num1 = 0; num1 < 9; num1++)
 	{
-		for (num2 = '0'; num2 <= 10; num2++)
+		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			if (!(num1 == num2) || (num2 > num1))
-			{
-				putchar(num2);
-				putchar(num1);
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
-				if (!(num1 == '9' && num2 == '8'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			if (num1 == 8 && num2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
 		}
 	}
 
