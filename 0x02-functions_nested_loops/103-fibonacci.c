e66ef9fe28b6e2e0730842c85447c750e8bb791a
Fash-ya <ishakufaith20@gmail.com>
2022-12-14 13:11:01 -0800

    103 fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sum_even = 0;
+	int i;
+	int j;
+
+	int add = 1;
+
+	i = 1;
+	j = 1;
+
+	while (j < 4000000)
+	{
+		add = i + j;
+		i = j;
+		j = add;
+		if  ((add <= 4000000) && (add % 2 == 0))
+			sum_even += add;
+	}
+	printf("%d\n", sum_even);
+
+	return (0);
+}
