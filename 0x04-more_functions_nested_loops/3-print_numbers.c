d660564e23beaf3b8167e43dcdc51b5dec432b74
Fash-ya <ishakufaith20@gmail.com>
2022-12-15 11:01:24 -0800

    3 print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int number = 48;
+
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
